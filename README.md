b156fe379a0ea53f080f72943853e2e1acdc2458
Mikerophonic <micgcon@gmail.com>
2023-07-02 14:51:26 -0700

    add UI and UI logic

---
+++ README.md
@@ -0,0 +1,18 @@
+Describe: TravelLog
+Test: "It makes a place constructor"
+Code: TravelLog()
+Input: let travelLog = new TravelLog();
+Output = Creates a travelLog construct
+
+
+
+Describe: addPlace
+Test: "It should add a new place to the Place object"
+Code: let place = new Place("NY", "July 2nd", "Notes");
+Output: Place {location: 'NY', time: 'July 2nd', notes: 'Notes'}
+
+
+Describe: assignId
+Test: "It should assign a new id to each place object"
+Code: let place = new Place("NY", "July 2nd", "Notes");
+Output: 
--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,10 @@ function TravelLog() {
   
 TravelLog.prototype.addPlace = function(place) {
     place.id = this.assignId();
-    this.places[place.Location] = place;
+    this.places[place.id] = place;
   };
 
-  TravelLog.prototype.assignId = function() {
+TravelLog.prototype.assignId = function() {
     this.currentId += 1;
     return this.currentId;
   };
@@ -29,6 +29,38 @@ function Place(location, time, notes) {
     this.notes = notes;
   }
   
-  Place.prototype.locationLog = function() {
+Place.prototype.locationLog = function() {
     return this.location + " " + this.time + " " + this.notes;
   };
+
+// User Interface Logic ---------
+
+let travelLog = new TravelLog();
+
+function listPlaces(travelLogToDisplay) {
+  let placesDiv = document.querySelector("div#places");
+  placesDiv.innerText =  null;
+  const ul = document.createElement("ul");
+  Object.keys(travelLogToDisplay.places).forEach(function(key) {
+    const place = travelLogToDisplay.findLog(key);
+    const li = document.createElement("li");
+    li.append(place.locationLog());
+    li.setAttribute("id", place.id);
+    ul.append(li);
+  });
+  placesDiv.append(ul);
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const inputtedPlace = document.querySelector("input#new-place").value;
+  const inputtedTime = document.querySelector("input#new-time").value;
+  const inputtedNotes = document.querySelector("input#new-notes").value;
+  let newPlace = new Place(inputtedPlace, inputtedTime, inputtedNotes);
+  travelLog.addPlace(newPlace);
+  listPlaces(travelLog);
+}
+
+window.addEventListener("load", function (){
+  document.querySelector("form#new-location").addEventListener("submit", handleFormSubmission);
+});
