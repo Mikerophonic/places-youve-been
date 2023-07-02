5edf2f09fc37248652e5b77c2efe5e6032b07126
Mikerophonic <micgcon@gmail.com>
2023-07-02 11:33:17 -0700

    add co-author
    Co-authored-by: Julien Lenaz <Julienlenaz96@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,38 @@ function travelLog() {
 /// Business Log for Places ///
 
 //----Creates new key in travelLog and asggins it a value //
-travelLog.prototype.newPlace = function(newPlace) {
-    this.place[place.Location] = place;
+travelLog.prototype.addNewPlace = function(newPlace) {
+    this.newPlace[newPlace.Location] = newPlace;
 };
 
 //----Creates newPlace object in travelLog//
-function newPlace(Location, Time, Notes) {
+function NewPlace(Location, Time, Notes) {
     this.Location = Location;
     this.Time = Time;
     this.Notes = Notes;
 }
 
-newPlace.prototype.Location = function () {
+newPlace.prototype.placeLog = function () {
     return this.Location + " " + this.Time + " " + this.Notes;
 };
+
+
+// Business Logic for TravelLog ---------
+function TravelLog() {
+    this.places = {};
+  }
+  
+TravelLog.prototype.addPlace = function(place) {
+    this.places[place.Location] = place;
+  };
+  
+// Business Logic for Places ---------
+function Place(location, time, notes) {
+    this.location = location;
+    this.time = time;
+    this.notes = notes;
+  }
+  
+  Place.prototype.locationLog = function() {
+    return this.location + " " + this.time + " " + this.notes;
+  };
